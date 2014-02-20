469fe1451ffb280a6af3f6b605149e765cf852e8
JavaEEBoy <WongZeonbong@gmail.com>
2014-02-20 20:33:56 +0800

    完成了提出遠程數據庫的核心代碼

--- PCTech/src/model/ParkingLotDAO.java
+++ PCTech/src/model/ParkingLotDAO.java
@@ -1,18 +1,51 @@
 package model;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class ParkingLotDAO {
+	private ApplicationContext applicationContext;
+	private JdbcTemplate jdbcTemplate;
+	private static final String SQL = "select * from parkingspot";
+	
 	public static ParkingLotDAO createInstance(){
 		return new ParkingLotDAO();
 	}
 	
-	public void connect(String parkingLotId){
-		
+	private void connectToDataSource(String parkingLotId){
+		applicationContext = new ClassPathXmlApplicationContext("remote-datasource.xml");
+		DataSource dataSource = (DataSource) applicationContext.getBean(parkingLotId);
+		jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 	
+	@SuppressWarnings("unchecked")
 	public void load(String parkingLotId,List<ParkingSpot> parkingSpots){
+		connectToDataSource(parkingLotId);
+		List<Map<String,Object>> results = jdbcTemplate.queryForList(SQL);
 		
+		for(Map<String,Object> data : results){
+			ParkingSpot ps = new ParkingSpot();
+			ps.setId((String)data.get("id"));
+			ps.setStatus((Character)data.get("status"));
+			ps.setEntranceTime((Date)data.get("entrancetime"));
+			ps.setDepartureTime((Date)data.get("departuretime"));
+			
+			parkingSpots.add(ps);
+		}
+	}
+
+	public void setApplicationContext(ApplicationContext applicationContext) {
+		this.applicationContext = applicationContext;
+	}
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
 	}
 	
 }
